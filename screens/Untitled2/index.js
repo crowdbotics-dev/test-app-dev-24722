1eb15d0c19f1eb1d81e7f52efcfa62d8115297cf
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2022-06-14 11:11:05 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,5 +1,3 @@
-import { TextInput } from "react-native";
-import { Switch } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -10,22 +8,7 @@ const Untitled2 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      <Switch style={{
-      left: 64,
-      top: 77,
-      position: "absolute",
-      width: 50,
-      height: 34,
-      transform: "rotate(45deg)"
-    }}></Switch><TextInput style={{
-      left: 47,
-      top: 192,
-      position: "absolute",
-      backgroundColor: "#ffffff",
-      borderColor: "#cccccc",
-      width: 150,
-      height: 30
-    }}></TextInput></View>;
+      </View>;
 };
 
 export default Untitled2;
